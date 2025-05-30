779b3bd02d146efe1ffe1ce60aaf2c776e93946f
toposTForm <bereslavskiy0@gmail.com>
2025-05-30 22:39:03 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TracksModule } from './tracks/tracks.module';
+import { ArtistsModule } from './artists/artists.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [UsersModule, TracksModule],
+  imports: [UsersModule, TracksModule, ArtistsModule, AlbumsModule],
   controllers: [AppController],
   providers: [AppService],
 })
